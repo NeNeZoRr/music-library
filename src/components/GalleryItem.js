4a68ca5b84ca9c7e5b6da1e7c9a9a07b3250aec6
NeNeZoRr <zorrillalin@gmail.com>
2023-11-28 17:04:53 -0500

    FinalActualRepoStart

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -43,7 +43,7 @@ function GalleryItem(props) {
 
     return (
         <div onClick={() => setView(!view)} style={{ 'display': 'inline-block' }}>
-            <p>One Gallery Item</p>
+            {/* This simple ternary shows the simple view when 'view' is false! */}
             {view ? detailView() : simpleView()}
         </div>
     )
