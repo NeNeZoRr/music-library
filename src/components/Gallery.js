5dd2c618ec5b92e2f0684b0f9b17c199b5d7432b
NeNeZoRr <zorrillalin@gmail.com>
2023-11-28 18:29:19 -0500

    Simplification and Polishing

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,17 +1,24 @@
-import GalleryItem from './GalleryItem'
+import React from 'react';
+import GalleryItem from './GalleryItem';
 
 function Gallery(props) {
+    const { data } = props;
 
-    const display = props.data.map((item, index) => {
-        return (
-            <GalleryItem item={item} key={index} />
-        )
-    })
-    return (
-        <div>
-            {display}
-        </div>
-    )
+    if (!data || data.length === 0) {
+        return <div>No items to display</div>;
+    }
+
+    const display = data.map((item) => (
+        <GalleryItem item={item} key={item.id} />
+    ));
+
+    const containerStyle = {
+        display: 'grid',
+        gridTemplateColumns: 'repeat(3, 1fr)',
+        gap: '20px', 
+    };
+
+return <div style={containerStyle}>{display}</div>;
 }
 
-export default Gallery
+export default Gallery;
--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -6,45 +6,55 @@ function GalleryItem(props) {
     const [isDetailView, setDetailView] = useState(false);
 
     const simpleStyle = {
-        width: '25vw',
-        height: '20vh',
+        width: 'auto',
+        height: 'auto',
         border: '1px solid black',
         margin: '2px',
     };
 
     const detailStyle = {
-        width: '80vw',
-        height: '20vh',
+        width: 'auto',
+        height: 'auto',
         border: '1px solid black',
         margin: '2px',
         backgroundImage: `url(${props.item.artworkUrl100})`,
         backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
+        backgroundSize: 'contain',
         color: 'yellow',
     };
 
+    const containerStyle = {
+        display: 'grid',
+        gridTemplateColumns: 'repeat(3, auto)',
+        gap: '10px', // Adjust the gap between items as needed
+    };
+
     const simpleView = () => (
-        <div style={simpleStyle}>
-            <h3>{props.item.trackName}</h3>
-            <h4>{props.item.collectionName}</h4>
+        <div style={containerStyle}>
+            <div style={simpleStyle}>
+                <h3>{props.item.trackName}</h3>
+                <h4>{props.item.collectionName}</h4>
+            </div>
         </div>
     );
 
     const detailView = () => (
-        <div style={detailStyle}>
-            <h2>{props.item.trackName}</h2>
-            <h3>
-                <Link to={`/artist/${props.item.artistId}`}>
-                    {props.item.artistName}
-                </Link>
-            </h3>
-            <h3>
-                <Link to={`/album/${props.item.collectionId}`}>
-                    {props.item.collectionName}
-                </Link>
-            </h3>
-            <h4>{props.item.primaryGenreName}</h4>
-            <h4>{props.item.releaseDate}</h4>
+        <div style={containerStyle}>
+            <div style={detailStyle}>
+                <h2>{props.item.trackName}</h2>
+                <h3>
+                    <Link to={`/artist/${props.item.artistId}`}>
+                        {props.item.artistName}
+                    </Link>
+                </h3>
+                <h3>
+                    <Link to={`/album/${props.item.collectionId}`}>
+                        {props.item.collectionName}
+                    </Link>
+                </h3>
+                <h4>{props.item.primaryGenreName}</h4>
+                <h4>{props.item.releaseDate}</h4>
+            </div>
         </div>
     );
 
