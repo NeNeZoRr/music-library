c535aaca41dc9cd84dd7d3c37249d5a407c0710d
NeNeZoRr <zorrillalin@gmail.com>
2023-11-28 16:47:25 -0500

    Fix of Commit

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,3 +1,4 @@
+
 import { useState } from 'react'
 
 function GalleryItem(props) {
@@ -43,7 +44,7 @@ function GalleryItem(props) {
 
     return (
         <div onClick={() => setView(!view)} style={{ 'display': 'inline-block' }}>
-            <p>One Gallery Item</p>
+            {/* This simple ternary shows the simple view when 'view' is false! */}
             {view ? detailView() : simpleView()}
         </div>
     )
