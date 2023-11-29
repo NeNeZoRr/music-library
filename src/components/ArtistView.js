7160c35779b6c19f6afd1fb8469c5dbfeb0cc89f
NeNeZoRr <zorrillalin@gmail.com>
2023-11-29 18:50:23 -0500

    adding server

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,13 +1,39 @@
-import { useParams } from 'react-router-dom';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom'; 
 
 function ArtistView() {
-    const { id } = useParams();
+    const [artistData, setArtistData] = useState([]);
+
+    const { id } = useParams(); 
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const url = `http://localhost:4000/album/${id}`;
+                const response = await fetch(url);
+                const data = await response.json();
+
+                const albums = data.results.filter(item => item.collectionType === 'Album');
+                setArtistData(albums);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, [id]);
+
+    const albumDisplay = artistData.map(album => (
+        <div key={album.collectionId}>
+            <p>{album.collectionName}</p>
+        </div>
+    ));
 
     return (
         <div>
-            <h2>The id passed was: {id}</h2>
             <p>Artist Data Goes Here!</p>
+            <p>ID: {id}</p>
+            {artistData.length === 0 ? <p>Loading...</p> : albumDisplay}
         </div>
     );
 }
